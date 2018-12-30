601e31fe364d56a9d70ff83386250d9572e70f86
vivi3968 <46255198+vivi3968@users.noreply.github.com>
2018-12-30 01:07:59 -0500

    Update URLComparator.java

--- URLComparator.java
+++ URLComparator.java
@@ -1,12 +1,5 @@
 import java.util.Comparator;
 
-/* Vivian Lam
- * ID: 111549991
- * vivian.lam@stonybrook.edu
- * Homework 7
- * CSE214, R11 (Reed Gantz)
- */ 
-
 /**
  * @author vivi3
  * Sort alphabetically ASCENDING based the URL of the WebPage 
